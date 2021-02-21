5fbd2abd021a1752518354feb28cc21df5e51597
Azderus <71942308+azderus@users.noreply.github.com>
2021-02-21 20:49:05 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
  $config = array( 
         'server' => 'irc.twitch.tv', 
         'port'   => 6667, 
-        'channel' => '#leaguesummary',
-        'name'   => 'mstkrc77', 
-        'nick'   => 'mstkrc77', 
-        'pass'   => 'oauth:ayxklnzv9zbt6tqacnvh6qp7jeqsuz', // get this from: http://twitchapps.com/tmi/
+        'channel' => '#kanal',
+        'name'   => 'ad', 
+        'nick'   => 'kadi', 
+        'pass'   => 'key',http://twitchapps.com/tmi/
 );
  
  
